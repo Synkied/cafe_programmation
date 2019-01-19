cfb6fe238ab1f64bf4c266dc3f90a524311a82a9
Synkied <synkx@hotmail.fr>
2019-01-19 18:11:46 +0100

    menu

--- frontend/src/components/Shared/Header.vue
+++ frontend/src/components/Shared/Header.vue
@@ -3,32 +3,34 @@
 
 <template>
   <div id="header">
-    <b-navbar toggleable="md" type="light" class="white-bg" fixed="top">
+    <div class="d-flex flex-nowrap w-100">
+      <b-navbar toggleable="l" type="light" class="white-bg" fixed="top">
 
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-      <b-navbar-brand href="#home">
-        <a class="navbar-brand js-scroll-trigger" href="#home" v-smooth-scroll="{ duration: 1000, offset: -50}">
-          <img id="nav-logo" src="../../assets/cafe_logo_solo.svg" alt="Café logo">
-        </a>
-      </b-navbar-brand>
+  <!--       <b-navbar-brand href="#home">
+    <a class="navbar-brand js-scroll-trigger" href="#home" v-smooth-scroll="{ duration: 1000, offset: -50}">
+      <img id="nav-logo" src="../../assets/cafe_logo_solo.svg" alt="Café logo">
+    </a>
+  </b-navbar-brand> -->
 
-      <b-collapse is-nav id="nav_collapse">
+        <b-collapse is-nav id="nav_collapse">
 
-        <ul id="menu">
-          <b-navbar-nav>
-            <li data-menuanchor="intentions"><a href="#intentions">Intentions</a></li>
-          </b-navbar-nav>
-          <b-navbar-nav>
-            <li data-menuanchor="realisations"><a href="#realisations">Réalisations</a></li>
-          </b-navbar-nav>
-          <b-navbar-nav>
-            <li data-menuanchor="contact" class="active"><a href="#contact">Contact</a></li>
-          </b-navbar-nav>
-        </ul>
+          <ul id="menu">
+            <b-navbar-nav class="text-left">
+              <li data-menuanchor="intentions"><a href="#intentions">Intentions</a></li>
+            </b-navbar-nav>
+            <b-navbar-nav class="text-left">
+              <li data-menuanchor="realisations"><a href="#realisations">Réalisations</a></li>
+            </b-navbar-nav>
+            <b-navbar-nav class="text-left">
+              <li data-menuanchor="contact" class="active"><a href="#contact">Contact</a></li>
+            </b-navbar-nav>
+          </ul>
 
-      </b-collapse>
-    </b-navbar>
+        </b-collapse>
+      </b-navbar>
+    </div>
   </div>
 </template>
 
