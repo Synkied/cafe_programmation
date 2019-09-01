7d001bf2cf2b369d008e8a29bb0500704008a92f
Synkied <synkx@hotmail.fr>
2019-09-01 21:56:27 +0200

    new model and csv_to_json update

--- projects/management/commands/csv_to_json.py
+++ projects/management/commands/csv_to_json.py
@@ -1,6 +1,6 @@
-import os, getopt, sys
-import pandas as pd
 import json
+import os
+import pandas as pd
 
 from django.core.management.base import BaseCommand
 
@@ -13,33 +13,34 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         input_file = options['i']
         if os.path.isfile(input_file):
-            print('file found', input_file)
+            print('file found')
+            print(input_file)
 
             csv_file_df = pd.read_csv(
                 input_file,
                 sep=",",
                 encoding="ISO-8859-1",
             )
-            csv_file_df['dimension'] = csv_file_df['dimension'].replace(['1-XS', '2-S'], 1)
-            csv_file_df['dimension'] = csv_file_df['dimension'].replace(['3-M'], 2)
-            csv_file_df['dimension'] = csv_file_df['dimension'].replace(['4-L', '5-XL'], 3)
+            csv_file_df['dimension'] = csv_file_df['dimension'].replace(['3A'], 1)
+            csv_file_df['dimension'] = csv_file_df['dimension'].replace(['3B'], 2)
+            csv_file_df['dimension'] = csv_file_df['dimension'].replace(['3C'], 3)
 
-            csv_file_df['avancement'] = csv_file_df['avancement'].replace(['1-ETU'], 1)
-            csv_file_df['avancement'] = csv_file_df['avancement'].replace(['2-2-CONS', '3-REAL'], 2)
-            csv_file_df['avancement'] = csv_file_df['avancement'].replace(['4-FAIT'], 3)
-            csv_file_df['avancement'] = csv_file_df['avancement'].replace(['5-ATT'], 4)
+            csv_file_df['avancement'] = csv_file_df['avancement'].replace(['4A'], 1)
+            csv_file_df['avancement'] = csv_file_df['avancement'].replace(['4B'], 2)
+            csv_file_df['avancement'] = csv_file_df['avancement'].replace(['4C'], 3)
+            csv_file_df['avancement'] = csv_file_df['avancement'].replace(['4D'], 4)
 
-            csv_file_df['fonction'] = csv_file_df['fonction'].replace(['1-MUSEO'], 2)
-            csv_file_df['fonction'] = csv_file_df['fonction'].replace(['2-BIBLIO'], 1)
-            csv_file_df['fonction'] = csv_file_df['fonction'].replace(['3-SPECT'], 3)
-            csv_file_df['fonction'] = csv_file_df['fonction'].replace(['4-FABR'], 4)
-            csv_file_df['fonction'] = csv_file_df['fonction'].replace(['5-MONUM'], 2)
+            csv_file_df['fonction'] = csv_file_df['fonction'].replace(['1A'], 2)
+            csv_file_df['fonction'] = csv_file_df['fonction'].replace(['1B'], 1)
+            csv_file_df['fonction'] = csv_file_df['fonction'].replace(['1C'], 3)
+            csv_file_df['fonction'] = csv_file_df['fonction'].replace(['1D'], 4)
 
-            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['1-MONUM'], 1)
-            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['2-REHAB'], 3)
-            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['3-NEUF'], 2)
+            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['2B'], 1)
+            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['2C'], 3)
+            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['2A'], 2)
 
             json_file = csv_file_df.to_json(orient="records")
+            print(json_file)
             fixture_list = []
             counter = 1
 
--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from search_filters.models import Fonction, Contexte, Dimension, Avancement
+from search_filters.models import Fonction
+from search_filters.models import Contexte
+from search_filters.models import Dimension
+from search_filters.models import Avancement
 # Create your models here.
 
 
@@ -11,13 +14,35 @@ class Project(models.Model):
     mission = models.TextField(_("mission"))
     concepteur = models.CharField(_("concepteur"), max_length=256)
     description = models.TextField(_("description"), max_length=362)
+    explicatif = models.TextField(
+        _("explicatif"),
+        max_length=362,
+        blank=True,
+        null=True
+    )
     lien_externe = models.URLField(_("lien externe"), blank=True, null=True)
     lien_pdf = models.FileField(_("lien pdf"), blank=True, null=True)
     image = models.ImageField(_("image"), blank=True, null=True)
-    fonction = models.ForeignKey(Fonction, null=True, on_delete=models.SET_NULL)
-    contexte = models.ForeignKey(Contexte, null=True, on_delete=models.SET_NULL)
-    dimension = models.ForeignKey(Dimension, null=True, on_delete=models.SET_NULL)
-    avancement = models.ForeignKey(Avancement, null=True, on_delete=models.SET_NULL)
+    fonction = models.ForeignKey(
+        Fonction,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+    contexte = models.ForeignKey(
+        Contexte,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+    dimension = models.ForeignKey(
+        Dimension,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+    avancement = models.ForeignKey(
+        Avancement,
+        null=True,
+        on_delete=models.SET_NULL
+    )
 
     def __str__(self):
         return self.titre
