3ec86d6dd020820aa8e5be9c369725c683bbb319
Synkied <synkx@hotmail.fr>
2019-09-01 22:04:14 +0200

    updates

--- projects/management/commands/csv_to_json.py
+++ projects/management/commands/csv_to_json.py
@@ -1,6 +1,6 @@
-import os, getopt, sys
-import pandas as pd
 import json
+import os
+import pandas as pd
 
 from django.core.management.base import BaseCommand
 
@@ -17,7 +17,7 @@ class Command(BaseCommand):
 
             csv_file_df = pd.read_csv(
                 input_file,
-                sep=";",
+                sep=",",
                 encoding="ISO-8859-1",
             )
             csv_file_df['dimension'] = csv_file_df['dimension'].replace(['3A'], 1)
@@ -34,9 +34,9 @@ class Command(BaseCommand):
             csv_file_df['fonction'] = csv_file_df['fonction'].replace(['1C'], 3)
             csv_file_df['fonction'] = csv_file_df['fonction'].replace(['1D'], 4)
 
-            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['2B'], 4)
-            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['2A'], 3)
-            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['2C'], 1)
+            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['2B'], 1)
+            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['2C'], 3)
+            csv_file_df['contexte'] = csv_file_df['contexte'].replace(['2A'], 2)
 
             json_file = csv_file_df.to_json(orient="records")
             fixture_list = []
@@ -52,23 +52,3 @@ class Command(BaseCommand):
 
             with open('C:\\Users\\PC\\Desktop\\projets_clients\\cafe_programmation\\projects\\fixtures\\test.json', 'w') as f:
                 json.dump(fixture_list, f)
-
-
-# "CRITERE 1 : Fonction
-#    1A : musée, patrimoine…
-#    1B : bibliothèque, numérique…
-#    1C : théâtre, spectacle…
-#    1D : fabrique, hybride…"
-#    "CRITERE 2 : Contexte
-#    2A : construction nouvelle
-#    2B : monument historique
-#    2C : batiment non patrimonial"
-#    "CRITERE 3 : Dimension
-#    3A : moins de 3 000 m²
-#    3B : entre 3 000 et 10 000 m²
-#    3C : plus de 10 000 m²"
-#    "CRITERE 4 : Avancement
-#    4A : à l'étude
-#    4B : en réalisation
-#    4C : réalisé
-#    4D : en attente ou abandonné"
